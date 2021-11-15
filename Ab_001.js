e0f8da820f12b43cd04fef015da56def14caa668
MulinhaGPlays <filaucraft.eu@gmail.com>
2021-11-15 19:49:50 -0300

    TrabalhoBiologia

---
+++ Ab_001.js
@@ -0,0 +1,11 @@
+const btn_play = document.querySelector(".btn_play");
+const logo = document.querySelector(".logo");
+const bg_video = document.querySelector(".bg_video");
+const bg_form = document.querySelector(".bg_form");
+const txt_1 = document.querySelector(".txt_1");
+const form_lyt2 = document.querySelector(".form_lyt-2");
+const form_lyt3 = document.querySelector(".form_lyt-3");
+
+let TXT_1 = "O sistema cardiovascular humano é formado pelos vasos sanguíneos, uma rede de tubos que transporta sangue pelo coração, uma bomba muscular responsável por impulsionar o sangue para o corpo. Esses órgãos trabalham juntos para garantir que todas as células do corpo recebam nutrientes e oxigênio.";
+
+let VAR_1 = 0;
